0cb3953511fa0dc5e9669d65daa1c3d0998fdd90
Archit Singh Chauhan <archit442singh@gmail.com>
2026-09-08 16:33:11 +0000

    inital commit

---
+++ src/main/java/com/archit/factory_machine_scheduler/FactoryMachineSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.archit.factory_machine_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryMachineSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryMachineSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archit/factory_machine_scheduler/FactoryMachineSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archit.factory_machine_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryMachineSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
